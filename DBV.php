d609b23891d50bddd696199aecb387172aba40ea
ehime <dodomeki@gmail.com>
2014-08-20 18:44:02 +0000

    Fixed issue with colons and apache_request_headers

--- DBV.php
+++ DBV.php
@@ -42,16 +42,18 @@ class DBV
 
     public function authenticate()
     {
+        $authorization = '';
+
         if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
             $authorization = $_SERVER['HTTP_AUTHORIZATION'];
         } else {
             if (function_exists('apache_request_headers')) {
                 $headers = apache_request_headers();
-                $authorization = $headers['HTTP_AUTHORIZATION'];
+                $authorization = explode(' ', base64_decode(substr($headers['Authorization'], 6)))[0];
             }
         }
 
-        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($authorization, 6)));
+        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = preg_split('/:/', $authorization, 2);
         if (strlen(DBV_USERNAME) && strlen(DBV_PASSWORD) && (!isset($_SERVER['PHP_AUTH_USER']) || !($_SERVER['PHP_AUTH_USER'] == DBV_USERNAME && $_SERVER['PHP_AUTH_PW'] == DBV_PASSWORD))) {
             header('WWW-Authenticate: Basic realm="DBV interface"');
             header('HTTP/1.0 401 Unauthorized');
